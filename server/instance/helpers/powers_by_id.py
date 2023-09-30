f6c6c3421a658676b7b892356039fb49c69a453c
Arnold-Mwangi <kirigwimwangi18@yahoo.com>
2023-09-30 21:46:49 +0300

    powers_by_id

--- server/instance/helpers/powers_by_id.py
+++ server/instance/helpers/powers_by_id.py
@@ -1,5 +1,5 @@
 from models import Power
-from flask import make_response
+from flask import make_response, jsonify
 
 def powers_by_id(id, app):
     try:
@@ -9,8 +9,8 @@ def powers_by_id(id, app):
             power_data = power.to_dict()
             return make_response(power_data, 200)
         else:
-            response_body = {"Power Not Found"}
-            return make_response(response_body, 500)
+            response_body = {"error":"Power Not Found"}
+            return make_response(jsonify(response_body), 404)
     except Exception as e:
         app.logger.error(f"Error in PowerById: {str(e)}")
         return({"error": "Serialization Error"}, 500)
