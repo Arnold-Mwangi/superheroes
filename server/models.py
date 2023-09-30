70a1b19191cb9cbf2536eeb8e2832fc986fe0d9a
Arnold-Mwangi <kirigwimwangi18@yahoo.com>
2023-09-30 21:40:13 +0300

    Added description validation

--- server/models.py
+++ server/models.py
@@ -55,8 +55,14 @@ class Power(db.Model, SerializerMixin):
 
     id = db.Column(db.Integer, primary_key = True)
     name = db.Column(db.String)
-    description = db.Column(db.String)
+    description = db.Column(db.String, nullable=False)
     created_at = db.Column(db.DateTime, server_default = db.func.now())
     updated_at = db.Column(db.DateTime, onupdate=db.func.now())
 
     heroes = db.relationship('HeroPower', back_populates='power')
+
+    @validates('description')
+    def validate_description(self,key, value):
+        if len(value) < 20 or value == "":
+            raise ValueError ("description must be present and at least 20 characters long")
+        return value
